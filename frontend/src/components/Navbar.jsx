6a7d75bcf97975c01054a5d345a5f76850c43578
Prit33 <prityadav1310@gmail.com>
2023-10-05 18:34:28 +0530

    Added Frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+
+function Navbar() {
+  const user = true;
+
+  return (
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+      <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
+      <div className="flex justify-center items-center space-x-0">
+        <p><BsSearch /> </p>
+        <input className='outline-none px-3' placeholder='Search a post' type="text" />
+      </div>
+
+      <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
+        {user? <h3><Link to="/write">Write</Link></h3> :<h3><Link to="/login">Login</Link></h3>}
+        {user ? <h3 to="">Profile</h3>:<h3><Link to="/register">Register</Link></h3>}
+      </div>
+      
+
+      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+        <h3><Link to="/login">Login</Link></h3>
+        <h3><Link to="/register">Register</Link></h3>
+      </div>
+    </div >
+    
+  )
+}
+
+export default Navbar
