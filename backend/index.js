9f0654c65824f7013389659b9116046c12bc1b9d
Prit33 <prityadav1310@gmail.com>
2023-10-11 18:40:27 +0530

    fixing cors v-3

--- backend/index.js
+++ backend/index.js
@@ -26,26 +26,26 @@ const connectDB = async () => {
 
 // middlewares
 dotenv.config();
-// app.use((req, res, next) => {
-//     res.setHeader(
-//         "Access-Control-Allow-Origin",
-//         "https://devforum-g0fi9yayn-prit33.vercel.app/"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//     );
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader("Access-Control-Allow-Private-Network", true);
-//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//     res.setHeader("Access-Control-Max-Age", 7200);
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://devforum-g0fi9yayn-prit33.vercel.app/"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
 
-//     next();
-// });
+    next();
+});
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: "https://devforum-3tiee5a8e-prit33.vercel.app", credentials: true }))
