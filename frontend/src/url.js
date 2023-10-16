8af840ab7516e748886d1a0cd49990833789ed76
Prit <74133659+Prit33@users.noreply.github.com>
2023-10-16 12:21:53 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,3 @@
 // export const URL=import.meta.env.VITE_URL
 export const URL="https://devforum-backend-testing.onrender.com"
-export const IF="http://localhost:5000/images/"
+export const IF="https://devforum-backend-testing.onrender.com/images/"
